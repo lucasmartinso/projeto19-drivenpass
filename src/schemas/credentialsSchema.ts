85fa93db016368489791ccbc8075cc576e0d4465
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-03-09 21:06:44 -0300

    feat: 422 error of credentials is personalizated

--- src/schemas/credentialsSchema.ts
+++ src/schemas/credentialsSchema.ts
@@ -1,8 +1,8 @@
 import joi from "joi"; 
 
 export const credentialsSchema = joi.object({ 
-    title: joi.string().min(3).max(30).required(), 
-    url: joi.string().uri().required(),
-    username: joi.string().min(4).max(30).required(), 
-    password: joi.string().pattern(/^[a-zA-Z0-9"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ".", ":", ";", "|","~","`","{","}","<",">","?","/"]{10,}/).required()
+    title: joi.string().min(3).max(30).required().label('Invalid title, must be between 3 and 50 characters'), 
+    url: joi.string().uri().required().label('Invalid url, has to be a url'),
+    username: joi.string().min(4).max(30).required().label('Invalid username, must be between 4 and 30 characters'), 
+    password: joi.string().pattern(/^[a-zA-Z0-9"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ".", ":", ";", "|","~","`","{","}","<",">","?","/"]{10,}/).required().label('Invalid title, must be at least 10 characters')
 })
