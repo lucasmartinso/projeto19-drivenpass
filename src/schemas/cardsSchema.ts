89c9ea2a855ace70e3e3052aabd4b04962be390d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-03-09 21:04:36 -0300

    feat: 422 error of card is personalizated

--- src/schemas/cardsSchema.ts
+++ src/schemas/cardsSchema.ts
@@ -1,12 +1,12 @@
 import joi from "joi"; 
 
 export const cardsSchema = joi.object({ 
-    title: joi.string().min(3).max(50).required(),
-    number: joi.string().pattern(/^[0-9]{16}$/).required(), 
-    name: joi.string().min(3).max(50).required(), 
-    cvc: joi.string().pattern(/^[0-9]{3}$/).required(), 
-    expirateDate: joi.string().pattern(/^(0[1-9]|10|11|12)\/[0-9]{2}$/).required(), 
-    password: joi.string().pattern(/^[0-9]{4,6}$/).required(), 
-    isVirtual: joi.boolean().required(), 
-    type: joi.string().valid("credit", "debit", "both").required()
+    title: joi.string().min(3).max(50).required().label('Invalid title, must be between 3 and 50 characters'),
+    number: joi.string().pattern(/^[0-9]{16}$/).required().label('Invalid card number, must have exactly 16 numbers'), 
+    name: joi.string().min(3).max(50).required().label('Invalid name, must be between 3 and 50 characters'), 
+    cvc: joi.string().pattern(/^[0-9]{3}$/).required().label('Invalid name, must have exactly 3 numbers'), 
+    expirateDate: joi.string().pattern(/^(0[1-9]|10|11|12)\/[0-9]{2}$/).required().label('Invalid expirate date, has to be a date'), 
+    password: joi.string().pattern(/^[0-9]{4,6}$/).required().label('Invalid password, must be between 4 and 6 characters'), 
+    isVirtual: joi.boolean().required().label('Invalid virtual type, has to be true or false'), 
+    type: joi.string().valid("credit", "debit", "both").required().label('Invalid type, has to be credit or debit or both')
 })
